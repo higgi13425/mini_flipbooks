116d90d739c602aa2bc51a652e78be7cf560cba1
Peter <higgi13425@yahoo.com>
2020-10-20 05:02:06 -0400

    fixed fhx facet labels in embed_flipbook

--- plot.R
+++ plot.R
@@ -5,9 +5,10 @@ data %>%
   aes(y = time_to_recurrence) + 
   geom_point() +
   geom_smooth(method = "lm") +
-  facet_grid(~ fam_hx) + 
+  facet_grid(~ fam_hx, 
+    labeller = labeller(fam_hx = label_both)) + 
   theme_minimal() +
   labs(x = "Preoperative Prostate Specific Antigen (PSA) level") +
   labs(y = "Months to Recurrence of Prostate Cancer") +
-  labs(title = "Recurrence of Prostate Cancer by Preop PSA")
+  labs(title = "Recurrence of Prostate Cancer by Preop PSA and Family History")
   
