af2788854c78fa71bf1899ef93a19700ce515e8d
Peter <higgi13425@yahoo.com>
2020-10-20 05:12:31 -0400

    updated wrangle example

--- wrangle.R
+++ wrangle.R
@@ -1,4 +1,8 @@
 data %>% 
   clean_names() %>% 
-  select(recurrence, fam_hx, age, aa, t_stage, units) %>% 
-  filter(units > 1)
+  select(recurrence, fam_hx, 
+    age, t_stage, units) %>% 
+  filter(units > 1) %>% 
+  select(-units) %>% 
+  arrange(desc(t_stage), 
+    desc(recurrence), age)
